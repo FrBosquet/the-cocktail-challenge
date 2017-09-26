2f03ba77fb9b4c41d31a1ecfcc5f7374850bdbaf
Fran Bosquet <frbosquet@gmail.com>
2017-09-26 12:39:38 +0200

    Done company description, working in company services

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import './style.scss'
+import $ from 'jquery'
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  // document.getElementById('app').innerHTML = "Loaded"
-  console.log('script loaded')
+const rndTextPromise = (minWords = 10, maxWords = minWords) => $.get({
+    url: `https://www.randomtext.me/api/gibberish/p-1/${minWords}-${maxWords}`
+  })
+
+const fillClass = (selector, minWords = 10, maxWords = minWords) => {
+  const elements = $(selector)
+  const content = Array(elements.length)
+    .fill(undefined)
+    .map(e => rndTextPromise(minWords, maxWords))
+
+  Promise.all(content)
+    .then(data => {
+      elements.each((i, elm)=>{
+        elm.innerHTML = data[i].text_out.replace(/<p>|<\/p>/g, '')
+      })
+    })
+}
+
+$(document).ready(()=>{
+  fillClass('.rnd-spec-desc')
+  fillClass('.rnd-spec-link', 3, 5)
+  fillClass('.rnd-comp-desc', 20)
 })
